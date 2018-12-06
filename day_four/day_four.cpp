6c8b1ec7ed5c926c31d66a0a2155b455a872a8f2
Evan Jones <evanjones4040@gmail.com>
2018-12-05 16:16:47 -0800

    day four complete

--- day_four/day_four.cpp
+++ day_four/day_four.cpp
@@ -1,187 +1,220 @@
 #include <iostream>
 #include <fstream>
-#include <bits/stdc++.h> 
 #include <boost/algorithm/string.hpp> 
 #include "boost/date_time/posix_time/posix_time.hpp"
 
-class guard 
-{
-    public:
-
-    guard(int guard_id)
-    {
-        id=guard_id;
-    }
-
-    void asleep_at(boost::posix_time::ptime t)
-    {
-        curr_time=t;
-    }
-
-    void wakeup_at(boost::posix_time::ptime t)
-    { 
-        // if(t.time_of_day().hours() != curr_time.time_of_day().hours())
-        // {
-        //     // this never actually occurs, easy!
-        // }
-
-        auto low = curr_time.time_of_day().minutes();
-        auto diff = std::abs(t.time_of_day().minutes()-low);
-
-        for(int i = 0; i < diff; i++)
-        {
-            auto key = low + i;
-            if(key > 60)
-            {
-                key = key - 60;
-            }
-            
-            auto search = mins.find(key);
-            if (search != mins.end()) 
-            {
-                search->second++;
-            } 
-            else 
-            {
-                mins.insert(std::pair<int,int>(key,1));
-            }
-        }
-
-        time_asleep = time_asleep + (t - curr_time);
-    }
-
-    int get_sleepiest_minute()
-    {
-        std::pair<int,int> sleepiest(0,0);
-
-        for(auto const& [key,val]:mins)
-        {
-            if(val > sleepiest.second)
-            {
-                sleepiest = {key,val};
-            }
-        }
-
-        return sleepiest.first;
-    }
-
-    bool operator == (guard compare)
-    { 
-        return compare.id==this->id;
-    }
-
-    int id;
-    boost::posix_time::time_duration time_asleep; 
-    boost::posix_time::ptime curr_time;
-    std::map<int, int> mins;
-};
-
-guard get_sleepiest_guard(std::vector<guard> guard_list)
-{
-    guard max_guard = guard_list.front();
-    boost::posix_time::time_duration max = max_guard.time_asleep;
-
-    for(std::vector<int>::size_type i = 0; i != guard_list.size(); i++) {
-        guard current_guard = guard_list.at(i);
-        if(current_guard.time_asleep > max){
-            max_guard = current_guard;
-        }
-    }
-
-    return max_guard;
-}
-
-class file_line 
-{
-    public:
-
-    file_line(std::string current_line)
-    {
-        time = boost::posix_time::time_from_string(current_line.substr(1,16));
-        orig = current_line;
-    }  
-
-    int get_guard_number()
-    {
-        std::vector<std::string> intermediate;
-        boost::split(intermediate,orig,boost::is_any_of("#"));
-
-        std::vector<std::string> guard_str;
-        boost::split(guard_str,intermediate.back(),boost::is_any_of(" "));
-
-        return std::stoi(guard_str.front());
-    }
-
+typedef struct {
     boost::posix_time::ptime time;
     std::string orig;
-};
+} line_t;
+
+typedef struct {
+    int id;
+    bool is_sleeping;
+    boost::posix_time::time_duration time_asleep; 
+    boost::posix_time::ptime asleep_at;
+    std::map<int, int> mins;
+} guard_t;
+
+typedef struct {
+    guard_t guard;
+    int key;
+    int val;
+} freq_t;
+
+freq_t most_freq_min_asleep(std::vector<guard_t> list)
+{
+    freq_t res = {list.front(),0,0};
+
+    for(std::vector<int>::size_type i = 0; i != list.size(); i++)
+    {
+        // get most frq min of curr
+        auto curr = list.at(i);
+        int m_key = 0;
+        int m_val = 0;
+
+        for(auto const& [key,val]:curr.mins)
+        {
+            if(val > m_val)
+            {
+                m_key = key;
+                m_val = val;
+            }
+        }
+
+        if(m_val > res.val)
+        {
+            res = {curr,m_key,m_val};
+        }
+    }
+
+    return res;
+}
+
+bool has_item(std::vector<guard_t> list, int id)
+{
+    for(std::vector<int>::size_type i = 0; i != list.size(); i++)
+    {
+        if(list.at(i).id == id)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+guard_t get_item(std::vector<guard_t> list, int id)
+{
+    guard_t item;
+    for(std::vector<int>::size_type i = 0; i != list.size(); i++)
+    {
+        if(list.at(i).id == id)
+        {
+            item = list.at(i);
+        }
+    }  
+    return item;
+}
+
+guard_t get_sleepiest_guard(std::vector<guard_t> list)
+{
+    auto max = list.front();
+    for(std::vector<int>::size_type i = 0; i != list.size(); i++)
+    {
+        auto curr = list.at(i);
+        if(curr.time_asleep > max.time_asleep)
+        {
+            max = curr;
+        }
+    }
+    return max;
+}
+
+int get_sleepiest_minute(guard_t item)
+{
+    std::pair<int,int> sleepiest(0,0);
+
+    for(auto const& [key,val]:item.mins)
+    {
+        if(val >= sleepiest.second)
+        {
+            sleepiest={key,val};
+        }
+    }
+
+    return sleepiest.first;
+}
+
+
 
 int main() 
 {
-
     std::string line;
     std::ifstream input ("input.txt");
-    std::vector<file_line> line_list;
-    std::vector<guard> guard_list;
+    std::vector<line_t> lines;
+    std::vector<guard_t> guards;
 
     if(!input.is_open())
     {
-        return 0;
+        return 1;
     }
 
     while(std::getline(input,line))
     {
-        file_line curr(line);
-        line_list.push_back(curr);
+        auto time = boost::posix_time::time_from_string(line.substr(1,16));
+        line_t curr = { time, line };
+        lines.push_back(curr);
     }
 
     input.close();
 
-    std::sort(line_list.begin(), line_list.end(), [](file_line a, file_line b){
+    std::sort(lines.begin(), lines.end(), [](line_t a, line_t b){
         return a.time < b.time;
     });
 
-    for(std::vector<int>::size_type i = 0; i != line_list.size(); i++)
+    for(std::vector<int>::size_type i = 0; i != lines.size(); i++)
     {
-        file_line curr = line_list.at(i);
+        auto curr = lines.at(i);
 
         if(curr.orig.find("#") != std::string::npos)
         {
-            // register a guard
-            auto num = curr.get_guard_number();
-            guard tmp(num);
+            // move guard to front
+            std::vector<std::string> intermediate;
+            boost::split(intermediate,curr.orig,boost::is_any_of("#"));
 
-            auto pos = std::find(guard_list.begin(), guard_list.end(), tmp);
-            if(pos != guard_list.end())
+            std::vector<std::string> guard_str;
+            boost::split(guard_str,intermediate.back(),boost::is_any_of(" "));
+
+            auto id = std::stoi(guard_str.front());
+
+            if(has_item(guards,id))
             {
-                // move existing guard to back of list
-                auto index = guard_list.begin() + std::distance(guard_list.begin(),pos);
-                std::rotate(index, index + 1, guard_list.end());
+                // move our guard to back
+                guard_t item = get_item(guards,id);
+                guards.push_back(item);
             }
             else
             {
-                // add new guard to list
-                guard_list.push_back(tmp);
+                // push new guard into vec
+                boost::posix_time::time_duration td;
+                boost::posix_time::ptime pt;
+                std::map<int,int> mins;
+                guard_t item = {id, false, td, pt, mins};
+                guards.push_back(item);
             }
         }
-        else if(curr.orig.find("falls asleep") != std::string::npos)
+        else if(curr.orig.find("asleep") != std::string::npos)
         {
-            // register latest guard fell asleep
-            guard_list.back().asleep_at(curr.time);
+            // register latest guard as asleep
+            guards.back().asleep_at = curr.time;
+            guards.back().is_sleeping = true;
         }
-        else if(curr.orig.find("wakes up") != std::string::npos)
+        else if(curr.orig.find("wakes") != std::string::npos)
         {
-            // register latest guard wakes up
-            guard_list.back().wakeup_at(curr.time);
+            // register latest guard as awake
+            // guards.back().asleep_at = curr.time;
+            guards.back().time_asleep = guards.back().time_asleep + (curr.time - guards.back().asleep_at);
+            guards.back().is_sleeping = false;
+
+            auto low = guards.back().asleep_at.time_of_day().minutes();
+            auto diff = std::abs(curr.time.time_of_day().minutes()-low);
+
+            for(int i = 0; i < diff; i++)
+            {
+                auto key = low + i;
+                if(key > 60)
+                {
+                    key = key - 60;
+                }
+                
+                auto search = guards.back().mins.find(key);
+                if (search != guards.back().mins.end()) 
+                {
+                    search->second++;
+                } 
+                else 
+                {
+                    guards.back().mins.insert(std::pair<int,int>(key,1));
+                }
+            }
         }
+        else
+        {
+            return 1;
+        }
+
     }
 
-    guard sleepiest_guard = get_sleepiest_guard(guard_list);
+    auto sleepiest = get_sleepiest_guard(guards);
+    auto sleepiest_min = get_sleepiest_minute(sleepiest);
 
-    std::cout<<sleepiest_guard.get_sleepiest_minute()<<std::endl
-             <<sleepiest_guard.id<<std::endl
-             <<sleepiest_guard.get_sleepiest_minute()*sleepiest_guard.id<<std::endl;
+    std::cout<<"sleepiest guard: "<<sleepiest.id<<std::endl
+             <<"sleepiest min: "<<sleepiest_min<<std::endl
+             <<sleepiest.id*sleepiest_min<<std::endl;
 
+    auto most_freq = most_freq_min_asleep(guards);
+
+    std::cout<<most_freq.guard.id * most_freq.key<<std::endl;
 
     return 0;
 }
